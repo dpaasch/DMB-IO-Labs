559e1a656fc5a1d1805d20453425df876307672d
dpaasch@my.wctc.edu <>
2013-04-13 14:29:50 -0500

    Update #3 - Changed the System.out.println fields in the TextWriter, to JOptionPane.showMessageDialog fields.

--- src/lab1/TextWriter.java
+++ src/lab1/TextWriter.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.swing.JOptionPane;
 
 /**
  * 3. Add a new record by appending the file. Then read all records back in and 
@@ -12,26 +13,6 @@ import java.io.PrintWriter;
  * @author Dawn Bykowski, dpaasch@my.wctc.edu
  */
 public class TextWriter implements WriterStrategy {
-     
-//     public static void main(String[] args) throws IOException {
-//        File dataFile = new File(File.separatorChar + "Temp" + File.separatorChar
-//                + "ContactList.txt");
-//        
-//        // This allows the data to be added to the file, not overwriting what
-//        // already exists
-//         boolean append = true;
-//         PrintWriter writer = new PrintWriter
-//                 (new BufferedWriter
-//                 (new FileWriter(dataFile, append)));
-//         // Add the new record
-//         writer.println();
-//         writer.println("Lara|Rebholz|487 Westfield Way|Pewaukee|WI|53072|reboholz.lara@gmail.com|262-691-4031");
-//         writer.close();
-//         System.out.println("Wrote to file: " + dataFile.getAbsoluteFile());
-////         // List the records after the add:
-//         System.out.println("These are the current records:");
-//         TextReader.main(args);      
-//     }
 
     @Override
     public void writeln(String line) {
@@ -50,13 +31,13 @@ public class TextWriter implements WriterStrategy {
          writer.println();
          writer.println("Pam|Tillis|418 Westfield Way|Pewaukee|WI|53072|mydaddysingstoo@gmail.com|262-691-0098");
          writer.close();
-         System.out.println("Wrote to file: " + dataFile.getAbsoluteFile());
+         JOptionPane.showMessageDialog(null, "Wrote to file: " + dataFile.getAbsoluteFile());
 //         // List the records after the add:
 //         System.out.println("These are the current records:");
          } catch (IOException ioe) {
              if (writer != null) {
                  writer.close();
-                 System.out.println(ioe.getMessage());
+                 JOptionPane.showMessageDialog(null, ioe.getMessage());
                  System.exit(1);
              }
          }
